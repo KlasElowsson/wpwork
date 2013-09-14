0ff4474fcd1cb82ebe93471c3eca45ef1084733e
Klas Elowsson <klas.elowsson@gmail.com>
2013-09-14 23:00:16 +0200

    bytt ordning på kr till efter summa korgsammanställningen

--- wp-content/plugins/wordpress-simple-paypal-shopping-cart/wp_shopping_cart.php
+++ wp-content/plugins/wordpress-simple-paypal-shopping-cart/wp_shopping_cart.php
@@ -675,7 +675,7 @@ function cart_not_empty()
 
 function print_payment_currency($price, $symbol, $decimal)
 {
-    return $symbol.number_format($price, 2, $decimal, ',');
+    return number_format($price, 2, $decimal, ',').$symbol;
 }
 
 function cart_current_page_url() {

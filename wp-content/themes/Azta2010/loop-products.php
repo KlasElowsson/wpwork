5c65204e167ee2fc4f99bfa520a24165c796aca0
Klas Elowsson <klas.elowsson@gmail.com>
2013-09-18 16:16:02 +0200

    Justerat theme azta2010 för listning av taxonomy products fltande 2 produkter per rad.

--- wp-content/themes/Azta2010/loop-products.php
+++ wp-content/themes/Azta2010/loop-products.php
@@ -14,47 +14,101 @@
  * @since Twenty Ten 1.2
  */
 ?>
+<?php 
+//$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ) 
+?>
+<?php $annons_cnt=0; //Används för att gruppera 3 o 3 vid annonser ?>  
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 
 
+
+  
+
+<?php if ($annons_cnt==0) : ?>
+      <div class="annons-grupp">
+<?php elseif ($annons_cnt%2==0) : ?>
+      </div><!-- slut annons-grupp -->
+      <div class="annons-grupp">  
+<?php endif; ?>  
+<?php $annons_cnt++ ;?>
+
+
+
+
+
 <!-- Här startar jag med mina rader -->
 <?php //the_meta(); ?> <!-- Skriv ut meta -->
-<?php get_post_custom(); ?><br>
+<?php get_post_custom(); ?>
 
-<?php 
-  $var1 = "Storlek | ".get_post_meta($post->ID, "Storlek", true);
-  $var2 = "Farg | ".get_post_meta($post->ID, "Farg", true);
-  $var3 = "";
-  
-  $price = get_post_meta($post->ID, "Pris", true);
-  $shipping = 0;
+<?php
+$col_term = 'Färg ';
+$lcol_term = ' ';
+$color_terms = wp_get_object_terms($post->ID, 'prod-col');
+if(!empty($color_terms)){
+  if(!is_wp_error( $color_terms )){
+    foreach($color_terms as $term){
+      $col_term .= ' | '.$term->name ;
+      $lcol_term .= ' | '.$term->name ;
+    }
+  }
+}
+$size_term = 'Storlek ';
+$lsize_term = ' ' ;
+$size_terms = wp_get_object_terms($post->ID, 'prod-size');
+if(!empty($size_terms)){
+  if(!is_wp_error( $size_terms )){
+    foreach($size_terms as $term){
+      $size_term .= ' | '.$term->name ; 
+      $lsize_term .= ' | '.$term->name ; 
+    }
+  }
+}
+$var1 = "Storlek | ".$lsize_term ;
+$var2 = "Färg | ".$lcol_term ;
+$var3 = "";
+
+$price = get_post_meta($post->ID, "Pris", true);
+$shipping = 0;
 ?>
 
+
 <?php if ( get_post_meta($post->ID, "Pris", true)) : // If a user has filled out their description, show a bio on their entries  ?>
+ 
+ <!-- Title with link if multi -->
+      <div id="post-<?php the_ID(); ?>" <?php post_class("annons-fl"); ?>>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+<!-- 
         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
           <h1 class="entry-title"><?php the_title(); ?></h1>
-
+-->
           <div class="entry-content">
             <?php the_content(); ?>
             <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
           </div><!-- .entry-content -->
 
           <div>
-            <div>
-              <ul class="post-meta">
-                <li>
+           
+              <!--<ul class="post-meta">
+                <li>-->
+                  <div>
                   <span class="post-meta-key">Pris: </span><?php echo get_post_meta($post->ID, "Pris", true); ?>
-                </li>
-                <li>
-                  <span class="post-meta-key">Storlek: </span><?php echo get_post_meta($post->ID, "Storlek", true); ?>
-                </li>
-                <li>
-                  <span class="post-meta-key">Färg: </span><?php echo get_post_meta($post->ID, "Farg", true); ?>
-                </li>
-              </ul>
-            </div><!-- Metadata -->
+                  </div>
+                <!--</li>
+                <li>..>-->
+                  <div>
+                  <span class="post-meta-key">Storlek: </span><?php echo $lsize_term; ?>
+                  </div>
+                <!--</li>
+                <li>-->
+                  <p>
+                  <span class="post-meta-key">Färg: </span><?php echo $lcol_term; ?>
+                  </p>
+                <!--</li>
+              </ul>-->
+            
           </div>
 <?php else : ?>
            Du måste ange ett pris för beräkningen  !!!! 
@@ -66,13 +120,23 @@ if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned
   the_post_thumbnail();
 } 
 ?>
-<p>
+<div>
 <?php
   $button_code = print_wp_cart_button_for_product(the_title(), $price, $shipping, $var1, $var2, $var3);
   echo $button_code; ?>
-</p>
+</div>
         </div><!-- #post-## -->
 
 
 <?php endwhile; // end of the loop. ?>
+<?php if ($annons_cnt > 0) :?>
+      </div><!-- slut annons-grupp -->
+<?php endif; ?>
 
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+        <div id="nav-below" class="navigation">
+          <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
+          <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+        </div><!-- #nav-below -->
+<?php endif; ?>

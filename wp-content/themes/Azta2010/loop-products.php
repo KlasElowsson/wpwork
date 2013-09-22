5ab3a1555443afe3994c413f4faecb63e5b8864c
Klas Elowsson <klas.elowsson@gmail.com>
2013-09-22 18:34:06 +0200

    Styrt upp navigeringen i produktsidorna

--- wp-content/themes/Azta2010/loop-products.php
+++ wp-content/themes/Azta2010/loop-products.php
@@ -135,7 +135,9 @@ if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned
 <?php /* Display navigation to next/previous pages when applicable */ ?>
 <?php if (  $wp_query->max_num_pages > 1 ) : ?>
         <div id="nav-below" class="navigation">
-          <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div>
-          <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
+          <!-- <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyten' ) ); ?></div> -->
+          <!-- <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div> -->
+          <div class="nav-previous"><?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Föregående Produktsida', 'twentyten' ) ); ?></div>
+          <div class="nav-next"><?php next_posts_link( __( 'Nästa Produktsida <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?></div>
         </div><!-- #nav-below -->
 <?php endif; ?>
